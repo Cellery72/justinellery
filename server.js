dd6596b20e2175462a4a51104d0274bcb5b9f7b9
Justin <justin_ellery@hotmail.ca>
2016-03-13 15:02:21 -0400

    Working on npm scripts

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var express = require('express');
 
 var app = express();
-
-app.use(express.static(__dirname + '/public'));
-
 var port = 1337;
+
+app.use(express.static(__dirname + '/src'));
 app.listen(port);
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/src/index.html'));
+});
 
 module.exports = app;
 console.log(process.env.NODE_ENV + ' server running at http://localhost:' + port);
